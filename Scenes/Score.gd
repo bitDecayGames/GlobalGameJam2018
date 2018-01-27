20b399e63e51928661bf52a2d17a04a2de683a27
Tristan Havelick <tristan@havelick.com>
2018-01-27 13:00:19 -0700

    Make score 4 digits

--- Scenes/Score.gd
+++ Scenes/Score.gd
@@ -8,12 +8,14 @@ func _ready():
 	pass
 	
 func _draw():	
-	if value > 999:
-		value = 999
+	if value > 9999:
+		value = 9999
 	var s = String(value)
+	var thousands = get_node("DigitThousands")
 	var hundreds = get_node("DigitHundreds")
 	var tens = get_node("DigitTens")
 	var ones = get_node("DigitOnes")
+	thousands.value = null
 	hundreds.value = null
 	tens.value = null
 	ones.value = null
@@ -28,7 +30,11 @@ func _draw():
 		hundreds.value = int(s[0])
 		tens.value = int(s[1])
 		ones.value = int(s[2])
-	
+	elif s.length() == 4:
+		thousands.value = int(s[0])
+		hundreds.value = int(s[1])
+		tens.value = int(s[2])
+		ones.value = int(s[3])
 	
 	
 	
