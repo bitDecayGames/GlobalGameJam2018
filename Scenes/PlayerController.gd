75e1e5c7da1b4491e23db5331abac5c4a73d128c
Logan <monday.hopscotch@gmail.com>
2018-01-27 09:33:29 -0800

    player movement working now ish

--- Scenes/PlayerController.gd
+++ Scenes/PlayerController.gd
@@ -7,15 +7,19 @@ var playerMovement=[[0,0,0,0,0,0],
 					[0,0,3,3,0,0],
 					[0,1,2,1,2,0],
 					[0,0,0,0,0,0]]
-			
+					
+var spriteMap=[]
+
+var spacing = 150
+
 var left_action = "ui_left"
 var right_action = "ui_right"
 var up_action = "ui_up"
 var down_action = "ui_down"
 var action_action = "ui_select"
 
-var playerPos = Vector2(3,1)
-			
+var playerPos = Vector2(1, 3)
+
 var keyMap = {}
 
 func _process(delta):
@@ -34,27 +38,27 @@ func _process(delta):
 			playerPos.x = targetPos
 	elif (Input.is_action_pressed(up_action) && !keyMap.has(up_action)):
 		keyMap[up_action] = true
-		targetPos = playerPos.y + 1
+		targetPos = playerPos.y - 1
 		if(_can_Move(playerPos.x, targetPos)):
 			playerPos.y = targetPos
 	elif (Input.is_action_pressed(down_action) && !keyMap.has(down_action)):
 		keyMap[down_action] = true
-		targetPos = playerPos.y - 1
+		targetPos = playerPos.y + 1
 		if(_can_Move(playerPos.x, targetPos)):
 			playerPos.y = targetPos
-			
-			
 			
 	for action in keyMap:
 		if !Input.is_action_pressed(action):
 			keyMap.erase(action)
+			
+	update_sprites()
 
 func _can_Move(targetX,targetY):
 	print("target position: X: ")
 	print( targetX)
 	print( " Y: " )
 	print( targetY)
-	if(playerMovement[targetX][targetY] != 0):
+	if(playerMovement[targetY][targetX] != 0):
 		print("Movement Successful")
 		return true
 	else:
@@ -62,28 +66,34 @@ func _can_Move(targetX,targetY):
 		return false
 	
 
+func update_sprites():
+	for row in range(playerMovement.size()):
+		for col in range(playerMovement[row].size()):
+			if playerPos.x == col && playerPos.y == row:
+				spriteMap[playerPos.y][playerPos.x].set_opacity(1)
+			elif spriteMap[row][col] != null:
+				spriteMap[row][col].set_opacity(0.4)
 
 func _ready():
 	set_process(true)
-	var x = 0
-	var y = 0	
 	
-	for row in playerMovement:
-		x = 0
-		for col in row:
+	spriteMap = []
+	for row in range(playerMovement.size()):
+		spriteMap.append([])
+		for col in range(playerMovement[row].size()):
+			spriteMap[row].append(null)
 			#texture to playerMovement mapping
 			#TODO
-			if(col != 0):
+			if(playerMovement[row][col] != 0):
 				var s = Sprite.new()
 				s.set_texture(load("res://icon.png"))
-				s.set_pos(Vector2(x,y))
+				s.set_pos(Vector2(col * spacing, row * spacing))
+				s.set_opacity(0.4)
 				self.add_child(s)
-			x += 150
-		y += 150
-		
-	
-			
-		
+				spriteMap[row][col] = s
+				
+	print(spriteMap)
+				
 #for x in range(width):
    # matrix[x]=[]
    # for y in range(height):
