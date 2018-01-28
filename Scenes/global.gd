45e89596f3993af1a74e65c1599bccb86ca4648d
Logan <monday.hopscotch@gmail.com>
2018-01-28 10:18:01 -0800

    bird poop system

--- Scenes/global.gd
+++ Scenes/global.gd
@@ -2,6 +2,14 @@
 
 extends Node
 
+var moveableSpace = 1 << 0
+var playerPresent = 1 << 1
+var extinguisherSpawned = 1 << 2
+var transformerSpawned = 1 << 3
+var onFire = 1 << 4
+var birdPoop = 1 << 5
+var transformerBlown = 1 << 6
+
 var playerMovement
 					
 func _ready():
@@ -11,3 +19,6 @@ func _ready():
 					[0,0,1,1,0,1,1,0,1,1,0,0],
 					[0,1,1,1,1,1,1,1,1,1,1,0],
 					[0,0,0,0,0,0,0,0,0,0,0,0]]
+					
+					
+					
